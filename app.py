a8bf67ed8aaee9e9a61978ee97b07232ea957988
mkam0904 <mamta.doshi@gmail.com>
2026-07-09 10:50:41 -0700

    fixed import for move of initial_msg

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import gradio as gr
 import traceback
 import logging
 
+from src.prompts import initial_message
 from config import client, PUSHOVER_USER, PUSHOVER_TOKEN, APIFOOTBALL_API_KEY
 from data_loader import load_documents
 from rag import RAGIndex
